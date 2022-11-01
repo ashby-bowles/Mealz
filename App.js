9982f706daa440723fbbb87511ecbde6ae61bd76
Ashby Bowles <ashby.bowles@willowtreeapps.com>
2022-11-01 09:13:35 -0400

    Latest

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import MealsOverScreen from "./screens/MealsOverviewScreen";
 import MealsDetailsScreen from "./screens/MealsDetailScreen";
 
 const Stack = createNativeStackNavigator();
+
+function DrawerNavigator() {
+  return;
+}
 
 export default function App() {
   return (
--- screens/MealsDetailScreen.js
+++ screens/MealsDetailScreen.js
@@ -1,32 +1,63 @@
-import { Text, View, Image, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Button,
+} from "react-native";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
+import IconButton from "../components/IconButton";
 import List from "../components/MealDetail/List";
 import { MEALS } from "../data/dummy-data";
+import { useLayoutEffect } from "react";
 
-function MealsDetailsScreen({ route }) {
+function MealsDetailsScreen({ route, navigation }) {
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
+  function headerButtonPressHandler() {
+    console.log("Pressed!");
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            onPress={headerButtonPressHandler}
+            color="grey"
+            icon="star"
+          />
+        );
+      },
+    });
+  }, [navigation, headerButtonPressHandler]);
+
   return (
-    <View>
+    <ScrollView>
       <Image
         source={{ uri: selectedMeal.imageUrl }}
         style={styles.Image}
       ></Image>
-      <Text>{selectedMeal.title}</Text>
+      <Text style={styles.Title}>{selectedMeal.title}</Text>
       <MealDetails
         duration={selectedMeal.duration}
         complexity={selectedMeal.complexity}
         cost={selectedMeal.affordability}
         textStyle={styles.DetailText}
       />
-      <Subtitle> Ingredients</Subtitle>
-      <List data={selectedMeal.ingredients}></List>
-      <Subtitle> Steps </Subtitle>
-      <List data={selectedMeal.steps}></List>
-    </View>
+      <View style={styles.OuterContainer}>
+        <View style={styles.DetailContainer}>
+          <Subtitle> Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients}></List>
+          <Subtitle> Steps </Subtitle>
+          <List data={selectedMeal.steps}></List>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -41,6 +72,8 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "black",
   },
+  DetailContainer: { maxWidth: "80%" },
+  OuterContainer: { alignItems: "center", paddingBottom: 30 },
   DetailText: { color: "black", fontSize: 14 },
   Ingredients: {},
   Steps: {},
